51d72c050f39c7a9c2a47eef606517f6e08cb7d9
MoustafaMagdy10 <moustafamohamedmagdy10@gmail.com>
2025-05-06 00:02:19 +0300

    implement loing and register logic with jwt service

--- src/main/java/com/erp/inventory/config/SecurityConfiguration.java
+++ src/main/java/com/erp/inventory/config/SecurityConfiguration.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Autowired
